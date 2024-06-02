175b0e410be904b69da49d7a82458e8c5d7e53f6
zhuheqqq <zhuheqin55@gmail.com>
2024-06-02 21:59:53 +0800

    在服务端实现服务调用(服务注册完成

--- main/main.go
+++ main/main.go
@@ -1,47 +1,58 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"geerpc"
-	"geerpc/codec"
+	GeeRPC "geerpc"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := GeeRPC.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
 	}
 	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String() //向通道addr中输送数据
+	addr <- l.Addr().String()
 	GeeRPC.Accept(l)
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
-
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() {
-		_ = conn.Close()
-	}()
+	client, _ := GeeRPC.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
-	_ = json.NewEncoder(conn).Encode(GeeRPC.DefaultOption)
-	cc := codec.NewGobCodec(conn)
+	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMectod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
 	}
+	wg.Wait()
 }
